15a90ee671d931d79b8a5d015a8a143c9166c695
otrezateL1pipisek <93143444+otrezateL1pipisek@users.noreply.github.com>
2024-04-19 21:43:19 -0500

    Recursion

--- Courses/Algorithms_and_Data_Structures/Recursion/Reversed_Linked_List.py
+++ Courses/Algorithms_and_Data_Structures/Recursion/Reversed_Linked_List.py
@@ -1,6 +1,33 @@
-def factorial(n):
-    if n <= 1:
-        return 1
-    return n * factorial(n-1)
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
-print(factorial(3))
+class Solution(object):
+    def __init__(self):
+        self.head = Node(1)
+        self.tail = self.head
+        self.tail.next = Node(2)
+        self.tail.next.next = Node(3)
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        if not head or not head.next:
+            return head
+        
+        new_head = self.reverseList(head.next)
+        front = head.next
+        front.next = head
+        head.next = None
+        return new_head.data
+    
+def main():
+    sol = Solution()
+    print(sol.reverseList(sol.head))
+
+if __name__ == "__main__":
+    main()
+    
