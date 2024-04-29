c59c60e677be317e60bdb6ff73b63a5744954b80
otrezateL1pipisek <93143444+otrezateL1pipisek@users.noreply.github.com>
2024-04-28 20:30:55 -0500

    Binary Search

---
+++ Courses/Algorithms_and_Data_Structures/Binary Search/Koko_Loves_Bananas.py
@@ -0,0 +1,17 @@
+import math
+
+class Solution:
+    def minEatingSpeed(self, piles , h ) -> int:
+        l, r = 1, max(piles)
+        res = max(piles)
+        while l <= r:
+            k = int((l + r) / 2)
+            hours = 0
+            for n in piles:
+                hours += math.ceil(n/k)
+            if hours <= h:
+                res = min(res, k)
+                r = k - 1
+            else:
+                l = k + 1
+        return res
