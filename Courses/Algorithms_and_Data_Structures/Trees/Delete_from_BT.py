e4a82d578c085d7bb4e0b49727ca667e91a9a50c
otrezateL1pipisek <93143444+otrezateL1pipisek@users.noreply.github.com>
2024-05-03 19:43:54 -0500

    Trees

---
+++ Courses/Algorithms_and_Data_Structures/Trees/Delete_from_BT.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def minV(self, root):
+            cur = root
+            while cur and cur.left:
+                cur = cur.left
+            return cur.val
+    def deleteNode(self, root, key):
+        """
+        :type root: TreeNode
+        :type key: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            else:
+                gg = self.minV(root.right)
+                root.val = gg
+                root.right = self.deleteNode(root.right, gg)
+            return root
+        return root
