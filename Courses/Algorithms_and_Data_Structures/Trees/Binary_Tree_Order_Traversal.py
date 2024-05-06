e734089e9645d090bbd1fe64e33f3f3754c5b153
otrezateL1pipisek <93143444+otrezateL1pipisek@users.noreply.github.com>
2024-05-05 19:57:11 -0500

    Trees

--- Courses/Algorithms_and_Data_Structures/Trees/Binary_Tree_Order_Traversal.py
+++ Courses/Algorithms_and_Data_Structures/Trees/Binary_Tree_Order_Traversal.py
@@ -1,4 +1,29 @@
-g = []
-g.append([])
-g.append(1)
-print(g)
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root :
+            return []
+        n = -1
+        res = []
+        queue = deque() 
+        queue.append(root)
+        while len(queue) > 0:
+            n += 1
+            res.append([])
+            for _ in range(len(queue)):
+                cur = queue.popleft()
+                res[n].append(cur.val)
+                if cur.left:
+                    queue.append(cur.left)
+                if cur.right:
+                    queue.append(cur.right)
+        return res
