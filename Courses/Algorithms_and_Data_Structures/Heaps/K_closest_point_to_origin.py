6b381cfd1d3bff015e50b83f7b991c75bf9ec42f
otrezateL1pipisek <93143444+otrezateL1pipisek@users.noreply.github.com>
2024-05-16 19:11:57 -0500

    Heaps

---
+++ Courses/Algorithms_and_Data_Structures/Heaps/K_closest_point_to_origin.py
@@ -0,0 +1,18 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        heap, res, hash = [] , [], {}
+        
+        heapq.heapify(heap)
+        for n in points:
+            x = pow(n[0],2) + pow(n[1],2)
+            heapq.heappush(heap, x)
+            if x in hash:    
+                hash[x].append(n)
+            else:
+                hash[x] = [n]
+        for n in range(k):
+            for i in hash[heapq.heappop(heap)]:
+                res.append(i)
+                if len(res) == k:
+                    return res
+        return res
